f08669bef53b57ac968a827710b1afa7bae1899e
vicentemorales <vmorales121@gmail.com>
2023-05-25 16:53:53 -0700

    game done. changed to on mouse down to make it faster

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react'
+import MoleContainer from './Components/MoleContainer'
+
+function App(){
+    let [score, setScore] = useState(0)
+
+    const createMoleHill = () => {
+        let hills = []
+        for (let i = 0; i < 9; i++) {
+            hills.push(
+                <MoleContainer
+                key={i}
+                setScore={setScore}
+                score={score} />
+            )
+        }
+    
+        return (
+            <div>
+                {hills}
+            </div>
+        )
+    }
+
+    return (
+        <div className="App">
+            <h1>React-a-Mole!</h1>
+            {score}
+            {createMoleHill()}
+        </div>
+    )
+}
+
+export default App
---
+++ src/Components/Mole.jsx
@@ -0,0 +1,24 @@
+import { useEffect } from 'react'
+import moleImg from '../assets/mole.png'
+
+const Mole = (props) => {
+
+    useEffect(() => {
+        let randSeconds = Math.ceil(Math.random() * 10000)
+        let timer = setTimeout(() => {
+            props.toggle(false)
+        }, randSeconds)
+        return () => clearTimeout(timer)
+    })
+
+    return (
+        <div>
+            <img style={{'width': '30vw'}} src={moleImg} onMouseDown={props.handleClick} />
+        </div>
+    )
+}
+
+export default Mole
+
+
+
